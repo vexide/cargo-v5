8b9a2503b817ae726cdbb1beee88e0a1183b91a5
Gavin-Niederman <gavinniederman@gmail.com>
2023-09-08 17:09:50 -0700

    fix: flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, numtide, ... }: (numtide.lib.eachDefaultSystem
-    (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      devShell.${system} = import ./shell.nix;
-    )
-  );
+  outputs = {
+    self,
+    nixpkgs,
+    numtide,
+    flake-utils,
+    ...
+  }: 
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in with pkgs; rec {
+          devShells.${system} = import ./shell.nix;
+        }
+      )
+    );
 }
