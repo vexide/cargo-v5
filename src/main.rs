52c4553802eab181fc5697fcc6dd1c276bfd4ef3
Gavin-Niederman <gavinniederman@gmail.com>
2023-09-12 15:05:24 -0700

    feat: build projects using cargo subcommand
    Co-authored-by: doinkythederp <doinkythederp@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use cargo::{
-    core::{Shell, Workspace},
-    ops::CompileOptions,
-    CargoResult, Config,
-};
-use clap::{Parser, Subcommand};
-use std::path::PathBuf;
+use cargo_metadata::camino::Utf8PathBuf;
+use clap::{Args, Parser, Subcommand};
+use std::{path::PathBuf, process::Command};
 
-#[derive(Parser)]
-struct Cli {
+#[derive(Parser, Debug)]
+#[clap(bin_name = "cargo")]
+enum Cli {
+    /// A cargo subcommand for generating flamegraphs, using inferno
+    #[clap(version)]
+    Pros(Opt),
+}
+
+#[derive(Args, Debug)]
+struct Opt {
     #[command(subcommand)]
     command: Commands,
 
@@ -15,77 +19,86 @@ struct Cli {
     path: PathBuf,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
     Build,
 }
 
-fn main() -> CargoResult<()> {
-    let args = Cli::parse();
+cargo_subcommand_metadata::description!("Builds a pros-rs project");
 
-    let config = Config::new(
-        Shell::new(),
-        std::env::current_dir().unwrap(),
-        home::cargo_home().unwrap(),
-    );
+fn cargo_bin() -> std::ffi::OsString {
+    std::env::var_os("CARGO").unwrap_or_else(|| "cargo".to_owned().into())
+}
 
-    let manifest_path = if args.path.is_relative() {
-        std::env::current_dir()
-            .unwrap()
-            .join(args.path)
-            .join("Cargo.toml")
-    } else {
-        args.path.join("Cargo.toml")
-    };
+const TARGET_PATH: &str = "target/armv7a-vexos-eabi.json";
 
-    let ws = Workspace::new(&manifest_path, &config)?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let Cli::Pros(args) = Cli::parse();
+    let target_path = args.path.join(TARGET_PATH);
 
-    if ws.is_virtual() {
-        panic!("Virtual workspaces are not supported")
-        // for ws in ws.members() {
-        //     if ws.
-        // }
-    }
+    let target = include_str!("armv7a-vexos-eabi.json");
+    std::fs::write(&target_path, target).unwrap();
 
     match args.command {
         Commands::Build => {
-            let compile_options =
-                CompileOptions::new(&config, cargo::util::command_prelude::CompileMode::Build)?;
+            let mut build_cmd = Command::new(cargo_bin());
+            build_cmd.stdout(std::process::Stdio::piped());
+            build_cmd.arg("build");
 
-            //TODO: THIS DOESN'T WORK. We need to set the target to custom target "armv7a-vexos-eabi"
+            build_cmd.arg("--message-format");
+            build_cmd.arg("json-render-diagnostics");
 
-            let comp = cargo::ops::compile(&ws, &compile_options)?;
+            build_cmd.arg("--manifest-path");
+            build_cmd.arg(args.path.join("Cargo.toml"));
 
-            for bin in comp.binaries {
-                println!("Stripping Binary: {}", bin.path.display());
-                let strip = std::process::Command::new("arm-none-eabi-objcopy")
-                    .args(&[
-                        "--strip-symbol=install_hot_table",
-                        "--strip-symbol=__libc_init_array",
-                        "--strip-symbol=_PROS_COMPILE_DIRECTORY",
-                        "--strip-symbol=_PROS_COMPILE_TIMESTAMP",
-                        "--strip-symbol=_PROS_COMPILE_TIMESTAMP_INT",
-                        bin.path.to_str().expect("Invalid binary name"),
-                        &format!("{}.stripped", bin.path.to_str().unwrap()), // We already panicked if file name contained invalid unicode, so we can unwrap.
-                    ])
-                    .spawn()
-                    .unwrap();
-                strip.wait_with_output().unwrap();
-                let elf_to_bin = std::process::Command::new("arm-none-eabi-objcopy")
-                    .args(&[
-                        "-O",
-                        "binary",
-                        "-R",
-                        ".hot_init",
-                        &format!("{}.stripped", bin.path.to_str().unwrap()),
-                        &format!("{}.bin", bin.path.to_str().unwrap()),
-                    ])
-                    .spawn()
-                    .unwrap();
-                elf_to_bin.wait_with_output().unwrap();
+            build_cmd.arg("--target");
+            build_cmd.arg(&target_path);
+
+            build_cmd.arg("-Zbuild-std=core,alloc,compiler_builtins");
+
+            let mut out = build_cmd.spawn().unwrap();
+            let reader = std::io::BufReader::new(out.stdout.take().unwrap());
+            for message in cargo_metadata::Message::parse_stream(reader) {
+                match message.unwrap() {
+                    cargo_metadata::Message::CompilerArtifact(artifact) => {
+                        if let Some(binary_path) = artifact.executable {
+                            strip_binary(binary_path);
+                        }
+                    }
+                    _ => {}
+                }
             }
         }
     }
 
     Ok(())
+}
+
+fn strip_binary(bin: Utf8PathBuf) {
+    println!("Stripping Binary: {}", bin.clone());
+    let strip = std::process::Command::new("arm-none-eabi-objcopy")
+        .args(&[
+            "--strip-symbol=install_hot_table",
+            "--strip-symbol=__libc_init_array",
+            "--strip-symbol=_PROS_COMPILE_DIRECTORY",
+            "--strip-symbol=_PROS_COMPILE_TIMESTAMP",
+            "--strip-symbol=_PROS_COMPILE_TIMESTAMP_INT",
+            &bin.as_str(),
+            &format!("{}.stripped", bin), // We already panicked if file name contained invalid unicode, so we can unwrap.
+        ])
+        .spawn()
+        .unwrap();
+    strip.wait_with_output().unwrap();
+    let elf_to_bin = std::process::Command::new("arm-none-eabi-objcopy")
+        .args(&[
+            "-O",
+            "binary",
+            "-R",
+            ".hot_init",
+            &format!("{}.stripped", bin),
+            &format!("{}.bin", bin),
+        ])
+        .spawn()
+        .unwrap();
+    elf_to_bin.wait_with_output().unwrap();
 }
