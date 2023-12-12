640a2afc68d2d6105341043e54af3210006c5c5d
doinkythederp <doinkythederp@icloud.com>
2023-12-11 20:00:07 -0800

    feat: add flag for simulator builds

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use cargo_metadata::{camino::Utf8PathBuf, Message};
 use clap::{Args, Parser, Subcommand};
 use fs_err as fs;
 use std::{
+    io::{self, ErrorKind},
     path::PathBuf,
-    process::{Command, Stdio, Child, exit}, io::{self, ErrorKind},
+    process::{exit, Child, Command, Stdio},
 };
 
 cargo_subcommand_metadata::description!("Manage pros-rs projects");
@@ -28,6 +29,8 @@ struct Opt {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Build {
+        #[clap(long, short)]
+        simulator: bool,
         #[clap(last = true)]
         args: Vec<String>,
     },
@@ -51,7 +54,9 @@ impl CommandExt for Command {
         self.spawn().map_err(|err| match err.kind() {
             ErrorKind::NotFound => {
                 eprintln!("error: command `{}` not found", command_name);
-                eprintln!("Please refer to the documentation for installing pros-rs on your platform.");
+                eprintln!(
+                    "Please refer to the documentation for installing pros-rs on your platform."
+                );
                 eprintln!("> https://github.com/pros-rs/pros-rs#compiling");
                 exit(1);
             }
@@ -65,14 +70,14 @@ const TARGET_PATH: &str = "target/armv7a-vexos-eabi.json";
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let Cli::Pros(args) = Cli::parse();
-    let target_path = args.path.join(TARGET_PATH);
+    let path = args.path;
     let mut build_cmd = Command::new(cargo_bin());
     build_cmd
         .arg("build")
         .arg("--message-format")
         .arg("json-render-diagnostics")
         .arg("--manifest-path")
-        .arg(args.path.join("Cargo.toml"));
+        .arg(path.join("Cargo.toml"));
 
     if !is_nightly_toolchain() {
         eprintln!("warn: pros-rs currently requires Nightly Rust features.");
@@ -81,67 +86,91 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     match args.command {
-        Commands::Build { args } => {
-            build_cmd.args(args);
-
-            let target = include_str!("armv7a-vexos-eabi.json");
-            fs::create_dir_all(target_path.parent().unwrap()).unwrap();
-            fs::write(&target_path, target).unwrap();
-            build_cmd.arg("--target");
-            build_cmd.arg(&target_path);
-
-            build_cmd
-                .arg("-Zbuild-std=core,alloc,compiler_builtins")
-                .stdout(Stdio::piped());
-
-            let mut out = build_cmd.spawn_handling_not_found().unwrap();
-            let reader = std::io::BufReader::new(out.stdout.take().unwrap());
-            for message in Message::parse_stream(reader) {
-                if let Message::CompilerArtifact(artifact) = message.unwrap() {
-                    if let Some(binary_path) = artifact.executable {
-                        strip_binary(binary_path);
-                    }
+        Commands::Build { simulator, args } => {
+            build(path, args, simulator, |path| {
+                if !simulator {
+                    strip_binary(path);
                 }
-            }
+            });
         }
         Commands::Simulate { args } => {
-            if !has_wasm_target() {
-                eprintln!("warn: simulation requires the wasm32-unknown-unknown target to be installed");
-                eprintln!("hint: this can be fixed by running `rustup target add wasm32-unknown-unknown`");
-                exit(1);
-            }
-
-            build_cmd.args(args);
-
-            build_cmd
-            .arg("--target")
-            .arg("wasm32-unknown-unknown")
-            .arg("-Zbuild-std=std,panic_abort")
-            .stdout(Stdio::piped());
-
-            let mut out = build_cmd.spawn_handling_not_found().unwrap();
-            let reader = std::io::BufReader::new(out.stdout.take().unwrap());
             let mut wasm_path = None;
-            for message in Message::parse_stream(reader) {
-                if let Message::CompilerArtifact(artifact) = message.unwrap() {
-                    if let Some(binary_path) = artifact.executable {
-                        wasm_path = Some(binary_path);
-                    }
-                }
-            }
-
+            build(path, args, true, |path| wasm_path = Some(path));
             let wasm_path = wasm_path.expect("pros-simulator may not run libraries");
 
             let mut connection = jsonl::Connection::new_from_stdio();
             pros_simulator::simulate(wasm_path.as_std_path(), move |event| {
                 connection.write(&event).unwrap();
             })
-                .await
-                .unwrap();
+            .await
+            .unwrap();
         }
     }
 
     Ok(())
+}
+
+fn build(
+    path: PathBuf,
+    args: Vec<String>,
+    for_simulator: bool,
+    mut handle_executable: impl FnMut(Utf8PathBuf),
+) {
+    let target_path = path.join(TARGET_PATH);
+    let mut build_cmd = Command::new(cargo_bin());
+    build_cmd
+        .current_dir(&path)
+        .arg("build")
+        .arg("--message-format")
+        .arg("json-render-diagnostics")
+        .arg("--manifest-path")
+        .arg(format!("{}/Cargo.toml", path.display()));
+
+    if !is_nightly_toolchain() {
+        eprintln!("warn: pros-rs currently requires Nightly Rust features.");
+        eprintln!("hint: this can be fixed by running `rustup override set nightly`");
+        exit(1);
+    }
+
+    if for_simulator {
+        if !has_wasm_target() {
+            eprintln!(
+                "warn: simulation requires the wasm32-unknown-unknown target to be installed"
+            );
+            eprintln!(
+                "hint: this can be fixed by running `rustup target add wasm32-unknown-unknown`"
+            );
+            exit(1);
+        }
+
+        build_cmd
+            .arg("--target")
+            .arg("wasm32-unknown-unknown")
+            .arg("-Zbuild-std=std,panic_abort")
+            .stdout(Stdio::piped());
+    } else {
+        let target = include_str!("armv7a-vexos-eabi.json");
+        fs::create_dir_all(target_path.parent().unwrap()).unwrap();
+        fs::write(&target_path, target).unwrap();
+        build_cmd.arg("--target");
+        build_cmd.arg(&target_path);
+
+        build_cmd
+            .arg("-Zbuild-std=core,alloc,compiler_builtins")
+            .stdout(Stdio::piped());
+    }
+
+    build_cmd.args(args);
+
+    let mut out = build_cmd.spawn_handling_not_found().unwrap();
+    let reader = std::io::BufReader::new(out.stdout.take().unwrap());
+    for message in Message::parse_stream(reader) {
+        if let Message::CompilerArtifact(artifact) = message.unwrap() {
+            if let Some(binary_path) = artifact.executable {
+                handle_executable(binary_path);
+            }
+        }
+    }
 }
 
 fn strip_binary(bin: Utf8PathBuf) {
@@ -187,7 +216,8 @@ fn has_wasm_target() -> bool {
         .arg("target")
         .arg("list")
         .arg("--installed")
-        .output() else {
+        .output()
+    else {
         return true;
     };
     let rustup = String::from_utf8(rustup.stdout).unwrap();
